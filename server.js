61af91edfae81127cbc37624725e1dacb0aae4fe
tanner johnson <tannerjohnson08@gmail.com>
2024-08-12 21:10:10 -0500

    fixing port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes/index.js');
 
-const PORT = process.env.port || 3001;
+const port = process.env.PORT || 3001;
 
 const app = express();
 
@@ -27,6 +27,6 @@ app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
-app.listen(PORT, () => {
-    console.log(`App listening at http://localhost:${PORT}`)
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)
 });
