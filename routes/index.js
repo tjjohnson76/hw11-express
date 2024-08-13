9821a081b85d2314ec4016b1dfddac2260ae5214
tanner johnson <tannerjohnson08@gmail.com>
2024-08-12 20:43:26 -0500

    delete and add notes working

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 
-// Import our modular routers for /tips and /feedback
 const notesRouter = require('./notes.route');
-
 
 router.use('/notes', notesRouter);
 
--- routes/notes.route.js
+++ routes/notes.route.js
@@ -1,1 +1,47 @@
-const router = require('express').Router();
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
+const { v4: uuidv4 } = require('uuid');
+const notes = require('express').Router();
+
+
+
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+});
+
+
+notes.post('/', (req, res) => {
+    // console.log(req.body)
+
+    const { title, text } = req.body;
+
+    if (req.body){
+
+        const newNote = {
+            title,
+            text,
+            id: uuidv4()
+        }
+
+        readAndAppend(newNote, './db/db.json');
+        res.json('Note added successfully');
+    } else {
+        res.error('Error in adding note')
+    }
+})
+
+notes.delete('/:id', (req, res) => {
+    // console.log('delete route being hit')
+    const noteId = req.params.id;
+    console.log(noteId)
+    readFromFile('./db/db.json')
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+            console.log(json);
+            const result = json.filter((note) => note.id !== noteId);
+            writeToFile('./db/db.json', result);
+            console.log(`Note ${noteId} has been deleted 🗑️`)
+        })
+})
+
+
+module.exports = notes;
